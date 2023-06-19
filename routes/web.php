971425df37eaff0d8d214a7d9e296ef9f98c69f5
kazushi07 <reddwarfthespaceship@gmail.com>
2023-06-19 22:32:53 +0900

    5-2 end

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,12 @@ Route::post('hello', 'App\Http\Controllers\HelloController@post');
 Route::get('hello/add', 'App\Http\Controllers\HelloController@add');
 Route::post('hello/add', 'App\Http\Controllers\HelloController@create');
 
+Route::get('hello/edit', 'App\Http\Controllers\HelloController@edit');
+Route::post('hello/edit', 'App\Http\Controllers\HelloController@update');
+
+Route::get('hello/del', 'App\Http\Controllers\HelloController@del');
+Route::post('hello/del', 'App\Http\Controllers\HelloController@remove');
+
 // Route::get('hello', function () {
 //     return view('hello.index');
 // });
