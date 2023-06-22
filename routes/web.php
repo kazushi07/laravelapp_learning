1cdc5982ca6319ce7d1dc7a784b265937e3c4f89
kazushi07 <reddwarfthespaceship@gmail.com>
2023-06-22 21:09:33 +0900

    6-3

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,12 @@ Route::post('person/find', 'App\Http\Controllers\PersonController@search');
 Route::get('person/add', 'App\Http\Controllers\PersonController@add');
 Route::post('person/add', 'App\Http\Controllers\PersonController@create');
 
+Route::get('person/edit', 'App\Http\Controllers\PersonController@edit');
+Route::post('person/edit', 'App\Http\Controllers\PersonController@update');
+
+Route::get('person/del', 'App\Http\Controllers\PersonController@delete');
+Route::post('person/del', 'App\Http\Controllers\PersonController@remove');
+
 // Route::get('hello', function () {
 //     return view('hello.index');
 // });
