fd5ba99e8c6b0b0f974e2ef2a044132fca7cdca2
kazushi07 <reddwarfthespaceship@gmail.com>
2023-06-25 12:28:56 +0900

    7-3

--- resources/views/layouts/helloapp.blade.php
+++ resources/views/layouts/helloapp.blade.php
@@ -13,7 +13,7 @@
                 border-bottom:solid 1px #ccc; color:#ccc }    
             th { background-color:#999; color:#fff; padding: 5px 10px; }
             td { border: solid 1px #aaa; color: #999; padding: 5px 10px; }
-        </style>        
+        </style>
     </head>
     <body>
         <h1>@yield('title')</h1>
