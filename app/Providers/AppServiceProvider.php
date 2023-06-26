a409aa47dd532609dea7d3d49c7bda34ff973ee2
kazushi07 <reddwarfthespaceship@gmail.com>
2023-06-26 21:59:19 +0900

    pagination css fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Pagination\Paginator; //追記
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Paginator::useBootstrap(); //追記
     }
 }
